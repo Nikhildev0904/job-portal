ff7f54740c5ac4c919584c554f20aff4c58222b5
Nikhil Dev <arrnikhil@gmail.com>
2025-05-19 19:48:40 +0530

    chaged backend api link

--- frontend/src/services/job.service.js
+++ frontend/src/services/job.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'http://job-portal-env.eba-kdasiabz.us-east-1.elasticbeanstalk.com';
 
 const api = axios.create({
   baseURL: API_URL,
